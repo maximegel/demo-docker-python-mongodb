a5b66e2f1ae1771e1ad5828874bcccccfd935f6f
Maxime Gélinas <maxime.gelinas@idmobiles.com>
2018-02-20 19:30:53 -0500

    Add MongoDB and complete `home.html`

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM python:2.7
-ADD . /app
-WORKDIR /app
+ADD . /demo
+WORKDIR /demo
 RUN pip install -r requirements.txt
--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask, redirect, url_for
+import os
+from flask import Flask, redirect, url_for, request, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'], 27017)
+db = client.demoDb
+
 
 @app.route('/')
-def todo():
-        return "Hello!"
+def home():
+        
+        _items = db.demoDb.find()
+        items = [item for item in _items]
+
+        return render_template('home.html', items=items)
 
 
-@app.route('/new', methods=['POST'])
-def new():
-        redirect(url_for('todo'))
+@app.route('/api/items', methods=['POST'])
+def createItem():
+
+        item = {
+                'description': request.form['description']
+        }
+        db.demoDb.insert_one(item)
+
+        return redirect(url_for('home'))
 
 if __name__ == "__main__":
         app.run(host='0.0.0.0', debug=True)        
